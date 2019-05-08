02c1a844a067c738be7cf936f8bc79300366dd80
gexx <944644308@qq.com>
2019-05-08 21:40:59 +0800

    员工列表功能实现

--- spring-boot-02-logging/src/main/java/com/xxx/springboot/controller/EmployeeCntroller.java
+++ spring-boot-02-logging/src/main/java/com/xxx/springboot/controller/EmployeeCntroller.java
@@ -1,17 +1,23 @@
 package com.xxx.springboot.controller;
 
+import com.xxx.springboot.dao.EmployeeDao;
+import com.xxx.springboot.entities.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Collection;
 
 @Controller
 public class EmployeeCntroller {
-
-
+    @Autowired
+    EmployeeDao EmployeeDao;
 
     @GetMapping("/emps")
-    public String list() {
-
-
+    public String list(Model model) {
+        Collection<Employee> employees = EmployeeDao.getAll();
+        model.addAttribute("emps", employees);
         return "emp/list";
     }
 }
